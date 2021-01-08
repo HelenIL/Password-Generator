c8d7c902e34f88bc627b1a43228d22df1da1ef62
HelenLindberg <helenilindberg@yahoo.com>
2021-01-07 22:11:40 -0600

    working new code

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
-var numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var userPassword = []
-//var specialChar = [','"','#','$','%','&',','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',']',']
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-//let helloDivgenerateBtn = document.getElementById('helloDiv')/
-//let classDiv = document.getElementsByClassName('classDiv')
+var charLength;
+var yesSpecial;
+var yesNumber;
+var yesUpper;
+var yesLower;
+
+specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+space = [];
+var userPassword = [];
+var pick;
+
+//converts lower to upper
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+letter2 = letter.map(toUpper);
+
+
+
+//var generatePassword = document.querySelector("#generate");
+//var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
-function writePassword() {
-  alert("Let's make a password!"); 
-  var getNumber = confirm('Do you want numbers in you password?')
-  var charLength = prompt('How many characters in your password?')
-  var getUpper = confirm('Do you want uppercase letters?') 
-  //var password = generatePassword();
+function generatePassword() {
+  charLength = parseInt(prompt("Choose between 8 and 128 characters for your password." ));
+  if (!charLength) {
+    alert("Must add value.");
+  } else if (charLength < 8 || charLength > 128) {
+    charLength = parseInt(prompt("You must choose between 8 and 128"));
+  } else {
+    yesSpecial = confirm("Would you like to include special characters?");
+    yesNumber = confirm("Would you like to include numbers?");
+    yesUpper = confirm("Would you like to include uppercase letters?");
+    yesLower = confirm("Would you like to include lowercase letters?");
+  };
   
-  console.log(getNumber); 
-  console.log(charLength);
+  
+  
+  //var getNumber = confirm('Do you want numbers in you password?')
+  //var getUpper = confirm('Do you want uppercase letters?') 
+  //var getLower = confirm('Do you want lowercase letters?')
+  
+
+   
+  
+  
+  
 
   if (getNumber) {
     console.log("getNumber is true")
@@ -26,7 +55,7 @@ function writePassword() {
         userPassword.push(numberList[i]); 
     }
     console.log(userPassword)
-   
+
   }
 
   if (getUpper) {
@@ -35,6 +64,14 @@ function writePassword() {
       userPassword.push(upperLetter[i]); 
   }
   }
+  if (getLower) {
+    console.log("getLower is true")
+    for(let i = 0;getLower.length > i; i++){
+      userPassword.push(numberList[i]);
+    }
+  }
+
+
 
   passwordText.innerHTML = userPassword;
   //passwordText.value = password;
@@ -43,10 +80,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//let 
-//var
-//const 
-
-
+//generateBtn.addEventListener("click", writePassword);
